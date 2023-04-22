5f80fc24ddbb41845f16093897929c0da7f4f35e
CPF\phanuwat.atc <phanuwat.atc@cpf.co.th>
2023-04-23 00:10:32 +0700

    feat(app.py) add response()

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List, Union , Dict , Any
 
 app = FastAPI()
-
+class Response(BaseModel):
+    data: Union[str, int, float, bool, None, Dict[str, Any], List[Any]]
+    success: bool = True
+    message: Union[str, None] = None
+    
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+async def index() -> Response:
+    return Response(message="ai services is online.")
